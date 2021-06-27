b41505b21b674bb71481418754c6bacce740041a
snenene <sennebels@gmail.com>
2021-06-27 17:21:32 +0200

    FIrst Commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <section className="one">
+<h1>Hello there</h1>
+        </section>
+<section className="two">
+  <h1>Second Page</h1>
+</section>
+<section className="three">
+  <h1>Third Page</h1>
+</section>
+<section className="four">
+  <h1>Fourth Page</h1>
+</section>
+      </div>
+    </div>
+  );
+}
