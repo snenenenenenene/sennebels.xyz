cedc2953e6e7ae51794628b98c087e9ef94de0ab
snenenenenenene <sennebels@gmail.com>
2021-11-29 13:09:31 +0100

    Cleaned _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
-import React, { useEffect } from "react";
-import About from "../Components/About";
-import Contact from "../Components/Contact";
-import Navbar from "../Components/Navbar";
-import Skills from "../Components/Skills";
-import Socials from "../Components/Socials";
-import toast, { Toaster } from 'react-hot-toast';
+import React from "react";
 import '../styles/globals.css'
-import Head from 'next/head'
-import Script from 'next/script'
 import Home from "./Home.tsx"
 
 function MyApp() {
