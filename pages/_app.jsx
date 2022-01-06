e524af158acb1dd32211230190a7eb3a48eae6f5
snenenenenenene <sennebels@gmail.com>
2022-01-06 11:25:54 +0100

    Meta tag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,32 +1,38 @@
 import React from "react";
-import '../styles/globals.css'
-import Home from "./index.tsx"
-import 'tailwindcss/tailwind.css'
-import Head from 'next/head'
+import "../styles/globals.css";
+import Home from "./index.tsx";
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
 
-function MyApp({Component, pageprops}) {
+function MyApp({ Component, pageprops }) {
   return (
     <main>
       <Head>
-      <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Senne Bels" />
+        <meta
+          name="twitter:description"
+          content="My Personal Portfolio ( and future blog) !"
+        />
+        <meta
+          name="twitter:image"
+          content="http://www.sennebels.xyz/profilepic.jpeg"
+        />
+        <meta name="twitter:site" content="@snenenenene" />
+        <meta name="twitter:creator" content="@snenenenene" />
+        <meta property="og:url" content="http://www.sennebels.xyz" />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="My Personal Portfolio ( and future blog) !"
+        />
+        <meta property="og:description" content="Who Am I?" />
+        <meta
+          property="og:image"
+          content="http://www.sennebels.xyz/profilepic.jpeg"
+        />
       </Head>
-      <Head>
-<meta name="twitter:title" content="Senne Bels"/>
-      </Head>
-      <Head>
-<meta name="twitter:description" content="My Personal Portfolio ( and future blog) !"/>
-      </Head>
-      <Head>
-<meta name="twitter:image" content="http://www.sennebels.xyz/profilepic.jpeg"/>
-      </Head>
-      <Head>
-<meta name="twitter:site" content="@snenenenene"/>
-      </Head>
-      <Head>
-<meta name="twitter:creator" content="@snenenenene"/>
-      </Head>
-      <Component
-      {...pageprops}/>
+      <Component {...pageprops} />
     </main>
   );
 }
