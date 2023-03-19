dbabb9bff18cc85b9b9dc46fd2dcada2b0111de5
snenenenenenene <sennebels@gmail.com>
2023-03-19 10:46:56 +0100

    fix: :art: throwing up

--- app/components/common/Analytics.tsx
+++ app/components/common/Analytics.tsx
@@ -1,30 +1,26 @@
 "use client";
-
 import { load, trackPageview } from "fathom-client";
 import { usePathname, useSearchParams } from "next/navigation";
-import { Suspense, useEffect } from "react";
+import { useEffect } from "react";
 
-function TrackPageView() {
+export default function Fathom() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   useEffect(() => {
     load("HSHELJGH", {
       includedDomains: ["sennebels.xyz"],
+      spa: "auto",
     });
-  }, []);
 
-  // Record a pageview when route changes
-  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
     trackPageview();
+
+    // Record a pageview when route changes
   }, [pathname, searchParams]);
 
   return null;
-}
-
-export default function Fathom() {
-  return (
-    <Suspense fallback={null}>
-      <TrackPageView />
-    </Suspense>
-  );
 }
