5c99481738347da6a78a06a45f73fe559208fe16
snenenenenenene <sennebels@gmail.com>
2023-04-16 18:28:30 +0200

    fix: :lipstick: fixed emoji offset

--- app/components/common/Marquee.tsx
+++ app/components/common/Marquee.tsx
@@ -10,9 +10,9 @@ export const Marquee = ({
 }) => {
   return (
     <main
-      className={`${className} font-bold relative  flex overflow-x-hidden border-y h-12 xs:h-16 sm:h-24 dark:border-dark-secondary border-light-secondary sm:text-7xl xs:text-5xl text-4xl items-center w-auto overflow-hidden`}
+      className={`${className} font-bold relative  flex overflow-x-hidden border-y h-12 xs:h-16 md:h-24 sm:h-12 dark:border-dark-secondary border-light-secondary md:text-7xl xs:text-5xl text-4xl items-center w-auto overflow-hidden`}
     >
-      <div className="py-12 animate-marquee align-bottom whitespace-nowrap items-center flex gap-x-4">
+      <div className="py-12 animate-marquee align-bottom whitespace-nowrap items-center flex md:gap-x-4">
         {wordList.map((word: string, i: number) => (
           <>
             <p key={word + i}>{word}</p>
@@ -21,7 +21,7 @@ export const Marquee = ({
         ))}
       </div>
 
-      <div className="absolute px-2 animate-marquee2 items-center whitespace-nowrap flex gap-x-4">
+      <div className="absolute px-2 animate-marquee2 items-center whitespace-nowrap flex md:gap-x-4">
         {wordList.map((word: string, i: number) => (
           <>
             <p key={word + i}>{word}</p>
@@ -78,7 +78,7 @@ export const ChangingEmoji = ({
 
   return (
     <p
-      className={`${className} px-8 text-4xl sm:text-6xl xs:text-5xl ${fadeClass}`}
+      className={`${className} md:px-8 px-3 -translate-y-1 text-3xl md:text-6xl xs:text-5xl ${fadeClass}`}
     >
       {emoji}
     </p>
