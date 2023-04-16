b59549b2b29716d3963ac5c610174024cd2acdab
snenenenenenene <sennebels@gmail.com>
2023-04-16 16:05:41 +0200

    feat: :sparkles: added proper emoji transitions to marquee

--- app/components/common/Marquee.tsx
+++ app/components/common/Marquee.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export const Marquee = ({
   wordList,
   className = "",
@@ -7,23 +10,77 @@ export const Marquee = ({
 }) => {
   return (
     <main
-      className={`${className} relative  flex overflow-x-hidden border-y h-12 xs:h-16 sm:h-24 dark:border-dark-secondary border-light-secondary sm:text-7xl xs:text-5xl text-4xl items-center w-auto overflow-hidden font-minecraft `}
+      className={`${className} font-bold relative  flex overflow-x-hidden border-y h-12 xs:h-16 sm:h-24 dark:border-dark-secondary border-light-secondary sm:text-7xl xs:text-5xl text-4xl items-center w-auto overflow-hidden`}
     >
       <div className="py-12 animate-marquee align-bottom whitespace-nowrap items-center flex gap-x-4">
         {wordList.map((word: string, i: number) => (
-          <p className="translate-y-2" key={word + i}>
-            {word}
-          </p>
+          <>
+            <p key={word + i}>{word}</p>
+            <ChangingEmoji emojiList={["🦥", "🍁", "🐌", "🐢"]} />
+          </>
         ))}
       </div>
 
       <div className="absolute px-2 animate-marquee2 items-center whitespace-nowrap flex gap-x-4">
         {wordList.map((word: string, i: number) => (
-          <p key={word + i} className=" text-center translate-y-2">
-            {word}
-          </p>
+          <>
+            <p key={word + i}>{word}</p>
+            <ChangingEmoji emojiList={["🦥", "🍁", "🐌", "🐢"]} />
+          </>
         ))}
       </div>
     </main>
+  );
+};
+
+export const ChangingEmoji = ({
+  emojiList,
+  className = "",
+}: {
+  emojiList: string[];
+  className?: string;
+}) => {
+  const [emoji, setEmoji] = useState(emojiList[0]);
+  const [fadeClass, setFadeClass] = useState("");
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //   }, 4000);
+  //   return () => clearInterval(interval);
+  // }, [emojiList]);
+
+  // iterate through the emojiList array and set the emoji state to the next emoji in the array by the index,while adding and removing the fade-in and fade-out classes
+
+  useEffect(() => {
+    let i = 0;
+    const interval = setInterval(() => {
+      setFadeClass("fade-out");
+      setTimeout(() => {
+        setEmoji(emojiList[i]);
+        setFadeClass("fade-in");
+      }, 1000); // adjust this timeout to control the duration of the animation
+      i++;
+      if (i === emojiList.length) {
+        i = 0;
+      }
+    }, 4000);
+    return () => clearInterval(interval);
+  }, [emojiList]);
+
+  // useEffect(() => {
+  //   setFadeClass("fade-out");
+  //   const timeout = setTimeout(() => {
+  //     setEmoji(emojiList[Math.floor(Math.random() * emojiList.length)]);
+  //     setFadeClass("fade-in");
+  //   }, 1000); // adjust this timeout to control the duration of the animation
+  //   return () => clearTimeout(timeout);
+  // }, [emoji]);
+
+  return (
+    <p
+      className={`${className} px-8 text-4xl sm:text-6xl xs:text-5xl ${fadeClass}`}
+    >
+      {emoji}
+    </p>
   );
 };
