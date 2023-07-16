5877f4efebed6d97395613aa8994ba9d438ab701
senne <sennebels@gmail.com>
2023-07-16 11:08:41 -0700

    feat: :package: updated next

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    images: {
-      // This will cause an error
-      allowFutureImage: true,
-    },
-  },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
