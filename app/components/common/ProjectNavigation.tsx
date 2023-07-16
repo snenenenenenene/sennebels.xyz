5fa35551dc1d2cbd61d0bddadd46363de17087e6
senne <sennebels@gmail.com>
2023-07-16 11:12:21 -0700

    style: :lipstick: border fix

--- app/components/common/ProjectNavigation.tsx
+++ app/components/common/ProjectNavigation.tsx
@@ -9,7 +9,7 @@ export const ProjectNavigation = ({
 }) => {
   return (
     <nav className="w-full h-[28rem] flex font-display text-[13rem]">
-      <section className="w-1/2 h-full border-t border-light-secondary border-r">
+      <section className="w-1/2 h-full border-y border-light-secondary dark:border-dark-secondary  border-r">
         <Link
           href={prevLink}
           className="w-full h-full dark:font-outline-dark-2 dark:text-dark-primary font-outline-2 text-light-primary dark:hover:font-outline-dark-0 hover:text-light-secondary dark:hover:text-dark-secondary hover:border-2 hover:font-outline-0 border-light-secondary flex justify-center items-center"
@@ -17,7 +17,7 @@ export const ProjectNavigation = ({
           <h2>PREV</h2>
         </Link>
       </section>
-      <section className="w-1/2 h-full border-t border-light-secondary">
+      <section className="w-1/2 h-full border-y border-light-secondary dark:border-dark-secondary">
         <Link
           href={nextLink}
           className="w-full h-full dark:font-outline-dark-2 dark:text-dark-primary font-outline-2 text-light-primary dark:hover:font-outline-dark-0 hover:text-light-secondary dark:hover:text-dark-secondary hover:border-2 hover:font-outline-0 border-light-secondary flex justify-center items-center"
